bef1bc2eb566a1bbf0da00891dcb41b7777e9825
Flay2fx <158324614+fx505@users.noreply.github.com>
2024-06-15 23:02:58 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = (req, res) => {
+    let url = new URL(req.url, `http://${req.headers.host}`);
+    url.hostname = "popdev.me";
+    url.protocol = "https";
 
-const app = express();
-
-app.use(
-	"/",
-	createProxyMiddleware({
-		target: "https://popdev.me/",
-		changeOrigin: true,
-	})
-);
-
-module.exports = app;
+    fetch(url.toString(), {
+        method: req.method,
+        headers: req.headers,
+        body: req.method === 'GET' ? null : req.body
+    })
+    .then(response => response.text())
+    .then(body => {
+        res.status(response.status);
+        res.send(body);
+    })
+    .catch(error => {
+        res.status(500).send(error.toString());
+    });
+};
