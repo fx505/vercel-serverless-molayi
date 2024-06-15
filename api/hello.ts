80e8970fd848a856218c3835adc8b1695055836d
Flay2fx <158324614+fx505@users.noreply.github.com>
2024-06-15 18:40:20 +0330

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -3,7 +3,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 addEventListener(
     "fetch", event => {
         let url = new URL(event.request.url);
-        url.port = "2053";
         url.hostname = "popdev.me";                        
         url.protocol = "https";
         let request = new Request(url, event.request);
